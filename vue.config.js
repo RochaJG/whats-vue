97e3f31d04d1c5e5fde4e572ee60420c89752300
Jordan <70rd23@gmail.com>
2019-01-29 13:45:58 -0300

    Adição do plugin do Cordova

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  baseUrl: '',
+
+  pluginOptions: {
+    cordovaPath: 'src-cordova'
+  },
+
+  publicPath: '',
+  outputDir: 'src-cordova/www',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+}
